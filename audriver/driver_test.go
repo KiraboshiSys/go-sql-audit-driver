1d8964e1c40b7b6288ed353694dd6594c71406d4
saebyeol <newhosi27@gmail.com>
2025-12-09 09:55:19 +0900

    chore: change module path

--- audriver/driver_test.go
+++ audriver/driver_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/mickamy/go-sql-audit-driver/audriver"
+	"github.com/KiraboshiSys/go-sql-audit-driver/audriver"
 )
 
 // TestAuditDriver_DirectExecution tests audit logging for direct SQL execution (non-transactional)
